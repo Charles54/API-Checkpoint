fe49ef84c6f2097bc0404b55e7c58b49918bdb2e
Prince Ndukwe <princendu54@gmail.com>
2023-06-08 13:51:18 +0100

    API

---
+++ src/FetchData.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios';
+
+function FetchData() {
+    const [records, setRecords] = useState([]);
+        
+        
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        //axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => setRecords(data))
+        .catch((err) => console.log(err))
+    }, [])
+
+
+  return (
+    <div> 
+        {records.map((list, index) => (
+                <li key={index}>{list.id} | {list.name} </li>
+            ))
+        }
+    </div>
+  )
+}
+
+export default FetchData
